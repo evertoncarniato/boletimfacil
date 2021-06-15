83cb288591a634d84c6423043f21c21981425493
Everton Carniato Firmino de Oliveira <everton.carniato@gmail.com>
2021-06-15 17:45:03 -0300

    Tentando implementar a store do VueX

---
+++ src/store/dados/state.js
@@ -0,0 +1,10 @@
+export default function () {
+  return {
+    alunosVinculados: [
+      { value: 0, label: 'Danielle Sayuri' },
+      { value: 1, label: 'Everton Carniato' },
+      { value: 2, label: 'João Eduardo' },
+      { value: 3, label: 'Maria Gabriela' }
+    ]
+  }
+}
